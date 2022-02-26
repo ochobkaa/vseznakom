7b119f63d7a011228d8591808cab396f085f1400
ochobkaa <creepersyndicate@yandex.ru>
2022-02-26 20:30:48 +0300

    вернул интерполяцию CurrentDomain в AuthLink

--- src/api/implicitFlow.ts
+++ src/api/implicitFlow.ts
@@ -1,3 +1,3 @@
 import CurrentDomain from "./currentDomain";
 
-export const AuthLink = "https://oauth.vk.com/authorize?client_id=8085643&display=page&redirect_uri=https://vseznakom.vercel.app/&display=page&scope=wall,groups&response_type=token&v=5.131";
+export const AuthLink = `https://oauth.vk.com/authorize?client_id=8085643&display=page&redirect_uri=${CurrentDomain}&display=page&scope=wall,groups&response_type=token&v=5.131`;
