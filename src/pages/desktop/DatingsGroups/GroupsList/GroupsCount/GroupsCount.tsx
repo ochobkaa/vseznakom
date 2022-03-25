6c995e2e3c911b4d33127b09b1d738ea4fb18b00
ochobkaa <creepersyndicate@yandex.ru>
2022-03-25 23:47:42 +0300

    Поправил счетчик

--- src/pages/desktop/DatingsGroups/GroupsList/GroupsCount/GroupsCount.tsx
+++ src/pages/desktop/DatingsGroups/GroupsList/GroupsCount/GroupsCount.tsx
@@ -3,11 +3,12 @@ import {Typography} from "@mui/material";
 import {GroupsCountSx} from "./GroupsCount.SxStyled";
 import GroupsCountProps from "./GroupsCount.Props";
 import getPronounedGroup from "./GroupsCount.getPronounedGroup";
+import getPronounedSearchStatus from "./GroupsCount.getPronounedSearchStatus";
 
 const GroupsCount = (props: GroupsCountProps) => {
     const {count} = props;
     const pronounedGroup = getPronounedGroup(count);
-    const pronounedSearchStatus = getPronounedGroup(count);
+    const pronounedSearchStatus = getPronounedSearchStatus(count);
 
     return (
         <Typography variant="h4" sx={GroupsCountSx}>
