4192a28febe2842c7e6263df46cd659068617f45
ochobkaa <creepersyndicate@yandex.ru>
2022-03-25 20:45:54 +0300

    Верстка списка групп часть 22

--- src/pages/desktop/DatingsGroups/GroupsList/DatingsGroup/GroupsList.SxStyled.ts
+++ src/pages/desktop/DatingsGroups/GroupsList/DatingsGroup/GroupsList.SxStyled.ts
@@ -2,7 +2,7 @@ import {SxProps, Theme} from "@mui/material";
 
 export const GroupsListSx: SxProps<Theme> = {
     display: "grid",
-    gridTemplateColumns: "repeat(auto-fit, 1fr)",
+    gridTemplateColumns: 'repeat(auto-fit, 1fr)',
     gridAutoRows: "1fr",
     gridAutoFlow: "column",
 }
