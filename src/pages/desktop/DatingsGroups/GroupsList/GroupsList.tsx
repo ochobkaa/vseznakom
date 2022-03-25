2decd8aa7f0b2b902248deabad105d978153bd58
ochobkaa <creepersyndicate@yandex.ru>
2022-03-26 01:09:31 +0300

    Добавил спинер при загрузке групп

--- src/pages/desktop/DatingsGroups/GroupsList/GroupsList.tsx
+++ src/pages/desktop/DatingsGroups/GroupsList/GroupsList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DatingsGroup from "./DatingsGroup";
-import {Box, Typography} from "@mui/material";
+import {Box, CircularProgress, Typography} from "@mui/material";
 import useDatingsGroups from "../../../../hooks/datingsGroups";
 import {GroupsListSx} from "./GroupsList.SxStyled";
 import GroupsCount from "./GroupsCount";
@@ -12,16 +12,19 @@ const GroupsList = () => {
 
     return (
         <>
-            <GroupsCount count={searchedGroups.length}/>
-            <Box sx={GroupsListSx}>
-                {searchedGroups.map(
-                    group => <DatingsGroup
-                        groupName={group.name}
-                        avatar={group.photo_100 ?? ""}
-                        url={`https://vk.com/${group.screen_name}`}
-                    />
-                )}
-            </Box>
+            {groups.length ? <>
+                <GroupsCount count={searchedGroups.length}/>
+                <Box sx={GroupsListSx}>
+                    {searchedGroups.map(
+                        group => <DatingsGroup
+                            groupName={group.name}
+                            avatar={group.photo_100 ?? ""}
+                            url={`https://vk.com/${group.screen_name}`}
+                        />
+                    )}
+                </Box>
+            </>
+            : <CircularProgress sx={{margin: "auto"}}/>}
         </>
     );
 };
